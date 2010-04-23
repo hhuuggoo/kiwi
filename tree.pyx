1ec81c7a9f2587afe667f920efbef2c40c192519
hugo  shi <hugoshi@bleb2.(none)>
2010-04-22 22:51:34 -0500

    needs more design

--- tree.pyx
+++ tree.pyx
@@ -66,28 +66,35 @@ cdef class Tree:
     cdef public np.ndarray target
     cdef public object data_descriptors
     cdef FieldDescriptor target_descriptor
+    
     def __cinit__(self, np.ndarray data, np.ndarray target, object data_descriptors, FieldDescriptor target_descriptor):
         self.data = data
         self.target = target
         self.target_descriptor = target_descriptor
+        
     def __init__(self,  np.ndarray data, np.ndarray target, object data_descriptors, FieldDescriptor target_descriptor):
         self.data_descriptors = data_descriptors
         
 cdef class SimpleBinaryTreeNode:
     cdef public Tree tree
-    cdef public np.ndarray sub_indexes
     cdef public int level
     cdef public SimpleBinaryTreeNode parent_node
+    cdef public bint store_data
     cdef object children
     cdef Rule rule
     
-    def __cinit__(self, Tree tree, np.ndarray sub_indexes, int level, SimpleBinaryTreeNode parent_node):
+    def __cinit__(self, Tree tree, int level,
+                  SimpleBinaryTreeNode parent_node, bint store_data):
         self.tree = tree
         self.sub_indexes = sub_indexes
         self.level = level
         self.parent_node = parent_node
-    def __init__(self, Tree tree, np.ndarray sub_indexes, int level, SimpleBinaryTreeNode parent_node):
+        self.store_data = store_data
+        
+    def __init__(self, Tree tree, int level,
+                 SimpleBinaryTreeNode parent_node, bint store_data):
         self.children=[]
+        
     cpdef SimpleBinaryTreeNode descend(self, np.ndarray sample):
         cdef bint idx
         if len(self.children)==0:
@@ -98,13 +105,20 @@ cdef class SimpleBinaryTreeNode:
                 return self.children[0].descend(sample)
             else:
                 return self.children[1].descend(sample)
-
-
-cpdef Rule tree_spit(SimpleBinaryTreeNode node, metric_func):    
+            
+    #Currently, we train using python functions, however we might take
+    #codes which determine which c functions to use later, for speed optimizations
+    
+    cpdef train(self, object metric_func=None, object stopping_func=None, int store_data=0):
+        cdef np.ndarray[np.int_t, ndim=1] idx1, idx2
+        if not stopping_func(self):
+            self.rule = tree_split(self, metric_func)
+            
+cpdef object tree_split(SimpleBinaryTreeNode node, metric_func, int store_data):    
     cdef np.ndarray[np.float64_t, ndim=2] data,target
     data=node.tree.data[node.sub_indexes,:]
     target=node.tree.target[node.sub_indexes]
-
+    cdef np.ndarray[np.int_t, ndim=1] idx1, idx2
     cdef int num_cols,cc,rc
     cdef double best_score,score,val,best_val
     cdef int best_column
@@ -112,41 +126,42 @@ cpdef Rule tree_spit(SimpleBinaryTreeNode node, metric_func):
     num_cols = data.shape[1]
     for cc in range(num_cols):
         if node.tree.data_descriptors[cc].discrete_int:
-            (val,score)=tree_split_column_discrete(data[:,cc], target, metric_func)
+            (val,score,idx1,idx2)=tree_split_column_discrete(data[:,cc], target, metric_func)
         else:
-            (val,score)=tree_split_column_continuous(data[:,cc],target,metric_func)
+            (val,score,idx1,idx2)=tree_split_column_continuous(data[:,cc],target,metric_func)
         if score>best_score:
             score=best_score
             best_column=cc
             best_val=val
     cdef FieldDescriptor best_field = node.tree.data_desciptors[cc]
     if best_field.discrete_int:
-        return DiscreteBinaryRule(best_field, best_val)
+        return (DiscreteBinaryRule(best_field, best_val), idx1, idx2)
     else:
-        return ContinuousBinaryRule(best_field, best_val)
+        return (ContinuousBinaryRule(best_field, best_val), idx1, idx2)
     
 
-cpdef double tree_split_column_discrete(np.ndarray input_data,
+cpdef object tree_split_column_discrete(np.ndarray input_data,
                                         np.ndarray input_target, metric_func):
     cdef np.ndarray[np.float64_t, ndim=2] data,target
     data=input_data
     target=input_target
     cdef np.ndarray[np.float64_t, ndim=1] discrete_values, target1, target2
-    cdef np.ndarray idx
+    cdef np.ndarray idx, best_idx
     cdef double x,score,best_score, best_discrete_class
     best_score=0.0
     discrete_values = np.unique1d(data)
     for x in discrete_values:
         idx = (data==x)
         target1=target[idx]
-        target2=target[-1 * idx]
+        target2=target[np.logical_not(idx)]
         score = metric_func(target1,target2)
         if score>best_score:
             score=best_score
-            best_discrete_class=x
-    return best_discrete_class
+            best_discrete_class = x
+            best_idx = idx
+    return (best_discrete_class, score, np.nonzero(best_idx), np.nonzero(np.logical_not(best_idx)))
 
-cpdef double tree_split_column_continuous(np.ndarray input_data,
+cpdef object tree_split_column_continuous(np.ndarray input_data,
                                           np.ndarray input_target, metric_func):
     
     cdef np.ndarray[np.float64_t, ndim=1] target1, target2, sorted_target,data,target
@@ -154,20 +169,20 @@ cpdef double tree_split_column_continuous(np.ndarray input_data,
     target=input_target
     cdef np.ndarray[np.int_t, ndim=1] sorted_idx
     cdef double score,best_score, best_value
-    cdef int x, idxlen
+    cdef int x, idxlen best_idx
     sorted_idx = np.argsort(data)
     sorted_target = target[sorted_idx]
     idxlen=len(sorted_idx)
     best_score=0.0
     for x in range(idxlen):
-        target1=target[:x]
-        target2=target[x:]
-        #score = metric_func(target1,target2)
-        score = mse_metric_c(target1,target2)
+        target1 = sorted_target[:x]
+        target2 = sorted_target[x:]
+        score = metric_func(target1,target2)
         if score>best_score:
             score=best_score
-            best_value=data[sorted_idx[x]]
-    return best_value
+            best_value = data[sorted_idx[x]]
+            best_idx = x
+    return (best_value, score, sorted_idx[:x], sorted_idx[x:])
 
 cdef double mse_metric_c(np.ndarray tdata1,
                     np.ndarray tdata2):
